7a72d7d96f91e4de53c8d7fb09d0a5f9d883638f
Macs94 <00309119@uca.edu.sv>
2020-03-17 06:12:21 +0100

    Add: Labo 01:Ejercicio 01

---
+++ Labo 01/Ejercicio 01/src/com/MACS/x00309119/Main.java
@@ -0,0 +1,22 @@
+package com.MACS.x00309119;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[]args){
+        Scanner sca= new Scanner(System.in);
+        int x = 0, y = 1;
+        System.out.println("Cuantos elementos?");
+        byte n = sca.nextByte();sca.nextLine();
+        System.out.println("Los primeros "+ n +" terminos son: ");
+        for(int i = 1;i <= n;i++){
+            int suma = 0;
+            System.out.print(x + ";");
+            suma =x + y;
+            x = y;
+            y = suma;
+
+        }
+    }
+}
+
