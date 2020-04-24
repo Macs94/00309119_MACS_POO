22882b095214470193d203619ef6e82093da4e28
Macs94 <00309119@uca.edu.sv>
2020-04-23 18:11:42 -0600

    Add:Labo_05_Ejercicio_01

---
+++ Labo 05/Ejercicio 01/Ejercicio 01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Ejercicio_01
+
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      
+      Console.WriteLine("Cuantos elementos?");
+      int n = int.Parse(Console.ReadLine());
+      int x = 0,y = 1;
+      for (int i = 0; i < n; i++)
+      {
+        int suma = 0;
+        Console.Write(x +";");
+        suma = x + y;
+        x = y;
+        y = suma;
+      }
+    }
+    
+  }
+}
