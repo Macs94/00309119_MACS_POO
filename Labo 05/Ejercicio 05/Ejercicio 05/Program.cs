06474e1b6c913d1339ca372d64ad1ce3d44fdc4d
Macs94 <00309119@uca.edu.sv>
2020-04-24 15:25:13 -0600

    Add:Labo_05_Ejercicio_05

---
+++ Labo 05/Ejercicio 05/Ejercicio 05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml;
+
+namespace Ejercicio_05
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList vocales = new ArrayList();
+            Console.WriteLine("Cuantas letras vas a ingresar?");
+            int n = Int32.Parse(Console.ReadLine());
+            int cont = 0;
+            Console.WriteLine("Ingresa las letras y se imprimiran las vocales");
+            while (n > cont)
+            {
+                char letra = Console.ReadLine()[0];
+                cont++;
+                if (letra == 'a' || letra == 'e' || letra == 'o' || letra == 'i' ||
+                    letra == 'u'||letra == 'A' || letra == 'E' || letra == 'O' || letra == 'I' || letra == 'U')
+                {
+                    vocales.Add(letra);
+                }
+            }
+            Console.WriteLine("Las vocales que ingresaste fueron "+ vocales.Count);
+            foreach (var i in vocales)
+            {
+                Console.Write(i);
+            }
+            {
+                
+            }
+        }
+    }
+}
