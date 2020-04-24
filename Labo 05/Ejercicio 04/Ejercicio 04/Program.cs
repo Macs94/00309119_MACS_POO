c290b85df176301628896f9e1219165406458b41
Macs94 <00309119@uca.edu.sv>
2020-04-24 13:44:29 -0600

    Add:Labo_05_Ejercicio_04

---
+++ Labo 05/Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String texto;
+            Console.Write("Escriba una cadena de texto: ");
+            texto = Console.ReadLine();
+            Console.WriteLine("Cambiando las lertras 'a' por 'b'");
+            texto = texto.Replace("a", "b");
+            Console.Write("La cadena de texto modificada es: "+ texto);
+        }
+    }
+}
