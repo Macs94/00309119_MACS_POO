fe1c17cd3100b58129cdd8d8612dab135668c22d
Macs94 <00309119@uca.edu.sv>
2020-04-23 20:19:23 -0600

    Add:Labo_05_Ejercicio_03

---
+++ Labo 05/Ejercicio 03/Ejercicio 03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos elementos?");
+            int n = int.Parse(Console.ReadLine());
+            int j = 1;
+            Secuencia(n,j);
+        }
+
+        public static void Secuencia(int n, int j)
+        {
+            if (j < n)
+            {
+                Console.Write(j);
+                Secuencia(n,j+1);
+            }
+            Console.Write(j);
+        }
+    }
+}
---
+++ Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
