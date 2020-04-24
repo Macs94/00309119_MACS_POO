34d2b9ba5766da0db11276109205879fd99e378d
Macs94 <00309119@uca.edu.sv>
2020-04-23 21:49:11 -0600

    Add:Meet2304

--- Meet/Meet2304/Meet2304/Program.cs
+++ Meet/Meet2304/Meet2304/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Meet2304
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meet2304
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //List<Persona> p = new List<Persona>();
+            var p = new List<Persona>();
+            p.Add(new Persona("Mario","Calix",25 ));
+            p.Add(new Persona("Felipe",edad:40 ));
+            p.Add(new Persona(edad: 26 ));
+            p.Add(new Persona(apellido:"Rodriguez" ));
+
+            String buscar = "Mario";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+            int añadir = 12;
+            int árbol = 14;
+
         }
+        
+
     }
 }
